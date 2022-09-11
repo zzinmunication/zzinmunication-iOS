34b85c3084dcb88ddc5b42153e73131777d13b58
yeonduing <yeonduing@gmail.com>
2022-09-11 15:19:47 +0900

    remove storyboard

--- zzinmunication/zzinmunication/MainViewController.swift
+++ zzinmunication/zzinmunication/MainViewController.swift
@@ -1,5 +1,5 @@
 //
-//  ViewController.swift
+//  MainViewController.swift
 //  zzinmunication
 //
 //  Created by itzel.du on 2022/09/11.
@@ -7,7 +7,7 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+final class MainViewController: UIViewController {
 
   override func viewDidLoad() {
     super.viewDidLoad()
@@ -16,4 +16,3 @@ class ViewController: UIViewController {
 
 
 }
-
